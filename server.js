735ddb83493e1fc642d798dece045daf668a5ac6
Atharva-Bakre <atharva.bakre05@gmail.com>
2026-02-23 23:32:40 +0530

    fixed render port

--- server.js
+++ server.js
@@ -69,6 +69,8 @@ app.post("/api/contact", async (req, res) => {
 });
 
 
-app.listen(3000, () =>
-    console.log("Server running on port 3000 🚀")
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT} 🚀`)
 );
